b1a85dbc25c29054f411aac316dd971819740898
Steve Dakin <sddakin@gmail.com>
2014-01-31 23:25:00 -0800

    card rotation and hand improvement improvements

--- app/js/app/MtGApp.js
+++ app/js/app/MtGApp.js
@@ -42,19 +42,21 @@ define(
 			self.showHand();
 		});
 
-		$(".handview .closebox").click(function() { self.hideHand(); });
+		$(".hand-closebox").click(function() { self.hideHand(); });
     };
 
     MtGApp.prototype.hideHand = function() {
     	var $handView = $(".handview");
     	$handView.hide();
     	$("#my-hand-nav").removeClass("active");
+        $(".hand-closebox").hide();
     };
 
     MtGApp.prototype.showHand = function() {
     	var $handView = $(".handview");
 		$handView.width($(".board").width() - parseInt($(".handview").css("left")) - 2);
 		$handView.show();
+        $(".hand-closebox").show();
     };
 
     return MtGApp;
@@ -62,11 +64,29 @@ define(
 
 function onCardClick(e) {
 	var $card = $(e.target);
+    var posOffset = ($card.height() - $card.width()) / 2;
 	var tapped = $card.attr("data-tapped") == "true";
 	if (tapped) {
-		$card.rotate({angle:90, animateTo:0, duration:1000});
+        var top = String(parseInt($card.css("top")) - posOffset) + "px";
+        var left = String(parseInt($card.css("left")) + posOffset) + "px";
+        $card.css("top", top);
+        $card.css("left", left);
+		$card.rotate({angle:90, animateTo:0, duration:750, callback:onCardUntapped});
 	} else {
-		$card.rotate({angle:0, animateTo:90, duration:1000});
+		$card.rotate({angle:0, animateTo:90, duration:750, callback:onCardTapped});
 	}
-	$card.attr("data-tapped", !tapped);
+
+    function onCardUntapped() {
+	    $card.attr("data-tapped", !tapped);
+    }
+
+    function onCardTapped() {
+        var originOffset = String($card.height() / 2) + "px";
+        $card.css("-webkit-transform-origin", originOffset + " " + originOffset);
+        var top = String(parseInt($card.css("top")) + posOffset) + "px";
+        var left = String(parseInt($card.css("left")) - posOffset) + "px";
+        $card.css("top", top);
+        $card.css("left", left);
+        $card.attr("data-tapped", !tapped);
+    }
 }
